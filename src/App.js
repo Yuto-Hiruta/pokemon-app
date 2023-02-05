5ba31a85db8aacb3700e843ac2c4a7e5c45cb89a
Yuto <xavi_soccerboy1030@icloud.com>
2023-02-04 23:50:29 -0800

    add description

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import './App.css';
 import {getAllPokemon} from "./utils/pokemon.js";
 
 function App() {
   const initialURL ="https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
@@ -11,10 +12,19 @@ function App() {
       //retrieve all Pokemon data
       let res = getAllPokemon(initialURL);
       console.log(res);
+      setLoading(false);
     };
     fetchPokemonData();
   }, []);
-  return <div className="App"></div>
+  return <div className="App">
+    {loading ? (
+    <h1>Loading....</h1>
+    ) : (
+      <> 
+      <h1>Got all Pokemon data</h1>
+      </>
+    )}
+  </div>
 }
 
 export default App;
