295f8dc2a5f4861f5297b642a81228510ae8e3c5
Yuto <xavi_soccerboy1030@icloud.com>
2023-02-05 17:53:18 -0800

    created new function

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import { useEffect, useState } from "react";
 import './App.css';
-import {getAllPokemon} from "./utils/pokemon.js";
+import {getAllPokemon, getPokemon} from "./utils/pokemon.js";
 
 function App() {
   const initialURL ="https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState();
 
   useEffect(() => {
     const fetchPokemonData = async () => {
 
       //retrieve all Pokemon data
       let res = getAllPokemon(initialURL);
-      console.log(res);
+      //get each pokemon's each data
+      loadPokemon(res.results);
       setLoading(false);
     };
     fetchPokemonData();
   }, []);
+
+  const loadPokemon = async(data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+      setPokemonData(_pokemonData);
+  };
   return <div className="App">
     {loading ? (
     <h1>Loading....</h1>
     ) : (
       <> 
-      <h1>Got all Pokemon data</h1>
+      <h1>Got all Pokemon data successfully</h1>
       </>
     )}
   </div>
